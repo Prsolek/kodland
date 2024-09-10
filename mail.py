095890b9bd31a53e3e7292b9cba1199136178aac
Prsolek <168739663+Prsolek@users.noreply.github.com>
2024-09-10 17:25:16 +0200

    Update mail.py

--- mail.py
+++ mail.py
@@ -10,26 +10,26 @@ slowa = {
     'SKIBIDI': '1. co jest dziwne jak skibidi toilet. 2. ala zabawne',
     'REL': 'określenie czegoś prawdziwego lub prawdziwego też wobec siebie',
     'ŚPIULKOLOT': 'stare określenie łóżka używane w 21w.',
-    'LIDLONKA': 'osoba która lubi Lidla i Biedronkę'
-    'BRUH': 'jako westchnienie wyrażające frustrację i niezadowolenie, czasem zdumienie, odpowiednik o, bracie'
-    'XD': 'śmieszne'
-    'Xd': 'ha ha... takie sobie'
-    'xd': 'EEeeeee... słabe'
+    'LIDLONKA': 'osoba która lubi Lidla i Biedronkę',
+    'BRUH': 'jako westchnienie wyrażające frustrację i niezadowolenie, czasem zdumienie, odpowiednik o, bracie',
+    'XD': 'śmieszne',
+    'Xd': 'ha ha... takie sobie',
+    'xd': 'EEeeeee... słabe',
     'xD': 'trochę to mnie rozbawiło'
 }
 
 while True:  # Pętla nieskończona
-    slowo = input('podaj słowo z caps lock oprócz rodzajów XD: ')
+    slowo = input('Podaj słowo z CAPS LOCK (oprócz rodzajów XD): ')
 
     if slowo in slowa:
         print(slowa[slowo])
     else:
-        print('Nie znam tego słowa.')
-        print(https://www.youtube.com/watch?v=xm3YgoEiEDc)
+        print('Nie znam tego słowa. Może spróbuj wpisać je inaczej?')
+        print('https://www.youtube.com/watch?v=xm3YgoEiEDc')
 
     for i in range(4, 0, -1):
         print(i)
         time.sleep(0.5)
 
     # Po odliczaniu program pyta o nowe słowo
-    slowo = input('podaj kolejne słowo z caps lock oprócz rodzajów XD: ')
+    slowo = input('Podaj kolejne słowo z CAPS LOCK (oprócz rodzajów XD): ')
